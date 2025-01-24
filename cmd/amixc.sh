5225266741fe4d084edf9be1af70087c227a7fc5
trindadedev13 <trindadedev13@gmail.com>
2025-01-24 02:08:22 -0300

    feat: improve cmd script

--- cmd/amixc.sh
+++ cmd/amixc.sh
@@ -2,16 +2,38 @@
 
 CACHE_DIR="$HOME/.amixc_cache"
 CACHE_FILE="$CACHE_DIR/amixc.jar"
+VERSION="1.0.0"
 
 if [ ! -f "$CACHE_FILE" ]; then
   mkdir -p "$CACHE_DIR"
-  wget https://github.com/trindadedev13/AmixC/releases/download/1.0.0/amixc.jar -O "$CACHE_FILE"
+  echo "Downloading resources..."
+  wget -q "https://github.com/trindadedev13/AmixC/releases/download/$VERSION/amixc.jar" -O "$CACHE_FILE" 2>/dev/null
 fi
 
-echo "Enter the verticalroot:"
-read verticalRoot
+if [ "$#" -lt 2 ]; then
+  echo "Usage: $0 -verticalroot <value> <files>"
+  exit 1
+fi
 
-echo "Enter the files (separate them with spaces):"
-read -a files
+verticalRoot=""
+files=()
+
+while (( "$#" )); do
+  case "$1" in
+    -verticalroot)
+      verticalRoot="$2"
+      shift 2
+      ;;
+    *)
+      files+=("$1")
+      shift
+      ;;
+  esac
+done
+
+if [ -z "$verticalRoot" ]; then
+  echo "Error: -verticalroot argument is required."
+  exit 1
+fi
 
 java -jar "$CACHE_FILE" -verticalroot "$verticalRoot" "${files[@]}"
